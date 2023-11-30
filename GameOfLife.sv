c1eda662db6b018f73cf52a129041f9b75a7b7ad
Maximilian Hallqvist <98323670+MaximilianHq@users.noreply.github.com>
2023-11-30 08:56:10 +0000

    Update GameOfLife.sv

--- GameOfLife.sv
+++ GameOfLife.sv
@@ -8,17 +8,49 @@ typedef struct{ // define new cell
 } entity;
 
 parameter GRIDSIZE = 10;
+parameter GENERATIONS = 10; // amount of simulation steps
+parameter DELAY = 100;
 
 typedef struct{ // define grid;
 	entity grid[GRIDSIZE][GRIDSIZE];
 } grid_layout;
 
 module GameOfLife ();
-	localparam GENERATIONS = 10; // amount of simulation steps
 	
 	// positions for surrounding neighbours
 	// packed array
 	int indices[8][2] = '{ '{-1, -1}, '{-1, 0}, '{-1, 1}, '{0, -1}, '{0, 1}, '{1, -1}, '{1, 0}, '{1, 1} };
+	
+	// create first grid
+	grid_layout grid_init;
+	
+	// array to store grid generation
+	grid_layout evolution[GENERATIONS];
+	
+	initial begin
+	
+		// initialize first grid
+		initiate_grid(grid_init);
+		
+		// save first generation
+		evolution[0] = grid_init;
+		
+		// run simulation
+		for (int gen = 1; gen < GENERATIONS; gen = gen + 1) begin
+			// copy previous generation
+			grid_layout grid_inst = evolution[gen-1];
+			// generate next generation
+	//		get_next_gen(grid_inst);
+			// store the new generation
+			evolution[gen] = grid_inst;
+			
+			#DELAY;
+		end	
+	end
+	
+	
+	
+	// FUNCTIONS //
 	
 	function void initiate_grid(ref grid_layout grid_inst);
 		// initialize every grid entity
@@ -31,7 +63,7 @@ module GameOfLife ();
 	endfunction
 	
 	function automatic void count_neighbours(
-	input grid_layout grid_inst, // maybe i dont have to include things that already are inside the module??? ex: grid
+	input grid_layout grid_inst,
 	input int i, j,
 	output int neighbours_alive
 	);
@@ -56,36 +88,16 @@ module GameOfLife ();
 	
 //	function get_next_gen(ref grid_layout grid);
 //		
-//		// game logic goes here...
-//		// call function get_neighbours
+
+		// create new grid layout
+			// for loop goers here
+//		// call function get neighbours 
+		// apply rules
+		// put new state in new grid
+		// assign new grid to old grid (maybe)
+		
+
 //		
 //	endfunction
-
-	// create first grid
-	grid_layout grid_init; // THIS IS NOT WORKING //
-	
-	// array to store grid generation
-	grid_layout evolution[GENERATIONS]; // THIS IS NOT WORKING //
-	
-	initial begin
-	
-		// initialize first grid
-		initiate_grid(grid_init);
-		
-		// save first generation
-		evolution[0] = grid_init;
-	
-	end
-	
-		
-	// run simulation
-	for (int gen = 1; gen < GENERATIONS; gen = gen + 1) begin
-		// copy previous generation
-		grid_layout grid_inst = evolution[gen-1];
-		// generate next generation
-//		get_next_gen(grid_inst);
-		// store the new generation
-		evolution[gen] = grid_inst;
-	end
 	
 endmodule
