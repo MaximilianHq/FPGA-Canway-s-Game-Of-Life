4fe175272493c0237d6b34ac42a7a042f5c2305a
Maximilian Hallqvist <maximilian.hallqvist@gmail.com>
2024-03-26 12:24:38 +0000

    Done for testing 1
    added cell-map history and checks for reset

--- GameOfLife.sv
+++ GameOfLife.sv
@@ -14,32 +14,27 @@ typedef struct {
 	coalition clan;
 } entity;
 
-parameter DIVIDE_DISPLAY_V = 20;	//one twentieth of the display
-parameter DIVIDE_DISPLAY_H = 20; //one twentieth of the display
+parameter DIVIDE_DISPLAY_V = 20;					// One twentieth of the display
+parameter DIVIDE_DISPLAY_H = 20;					// One twentieth of the display
 parameter GRIDWIDTH = 32;
 parameter GRIDHEIGHT = 24;
-
-// -- VGA Controller Physical Validation --
-// Instantiate the VGA controller module for physical validation.
-// (The controller has not been written yet)
-// The module displays three different colors to the display,
-// which can be modified by the onboard slide switches.
-// Note that not all colors can be chosen.
+parameter CELLBUFFER = 5; 							// Amount of cells-maps to store
+parameter ALLOWEDSTABLE = 10;						// Allowed stable cells at one time
 
 module VGA_PRIMA (
 	input entity cells[GRIDHEIGHT][GRIDWIDTH],
-	input 			      MAX10_CLK1_50, // 50 MHz clock - PIN_P11
-	input logic 			rst_sync,   // Push button  - PIN_A7
-	output logic [3:0] VGA_R,  // VGA Red Data   - PIN_AA1, V1, Y2, Y1
-	output logic [3:0] VGA_G,  // VGA Green Data - PIN_W1, T2, R2, R1
-	output logic [3:0] VGA_B,  // VGA Blue Data  - PIN_P1, T1, P4, N2
-	output logic       VGA_HS, // VGA hsync - PIN_N3
-	output logic       VGA_VS  // VGA vsync - PIN_N1
+	input MAX10_CLK1_50,								// 50 MHz clock - PIN_P11
+	input logic rst_sync,							// Push button  - PIN_A7
+	output logic [3:0] VGA_R,						// VGA Red current_state   - PIN_AA1, V1, Y2, Y1
+	output logic [3:0] VGA_G,						// VGA Green current_state - PIN_W1, T2, R2, R1
+	output logic [3:0] VGA_B,						// VGA Blue current_state  - PIN_P1, T1, P4, N2
+	output logic VGA_HS,								// VGA hsync - PIN_N3
+	output logic VGA_VS								// VGA vsync - PIN_N1
 );
 
 
 	// -- Variables --
-	logic rst_n;              // Synchronized asynchronous reset
+	logic rst_n;										// Synchronized asynchronous reset
 	// Pixel clock generator output
 	logic pixel_clock;
 	// Sync pulse generator connections
@@ -47,9 +42,9 @@ module VGA_PRIMA (
 	logic vblank_n;
 	logic blank_n;
 	// Local use
-	logic [9:0] pixel_count; // Count pixel to divide display into thirds
-	logic [8:0] lines;			// Count number of lines
-	logic [3:0] rgb [2:0];   // Colors to be displayed Red = MSB, Blue = LSB. 4-bit colors.
+	logic [9:0] pixel_count;						// Count pixel to divide display into thirds
+	logic [8:0] lines;								// Count number of lines
+	logic [3:0] rgb [2:0];							// Colors to be displayed Red = MSB, Blue = LSB. 4-bit colors.
 
 	// -- Continuous assignments --
 	// Drive color output low if either blank signal is active. Otherwise, display color.
@@ -78,10 +73,10 @@ module VGA_PRIMA (
 							rgb[1] = {1'h0,1'h0,1'h0,1'h0};
 							rgb[0] = {1'h0,1'h0,1'h0,1'h0};
 						end
-						break; // exit the j for loop
+						break;							// Exit the j for loop
 					end
 				end
-				break; // exit the i for loop
+				break;									// Exit the i for loop
 			end
 		end
 	end
@@ -91,7 +86,7 @@ module VGA_PRIMA (
 	always_ff @ (posedge pixel_clock or negedge rst_n) begin : PixelCount
 		if (~rst_n) begin
 			pixel_count <= '0;
-		end else if (blank_n) begin // Not blanking display
+		end else if (blank_n) begin				// Not blanking display
 			pixel_count <= pixel_count + 10'd1;
 		end else begin
 			pixel_count <= '0;
@@ -112,31 +107,56 @@ module VGA_PRIMA (
 endmodule
 
 
-module seed_generator(
-    input logic clk,
-    output entity rand_colony[GRIDHEIGHT][GRIDWIDTH]
+module GenerateSeed(
+	input logic clk,
+	input logic rst,
+	output entity new_colony[GRIDHEIGHT][GRIDWIDTH]
 );
 
 	logic [GRIDWIDTH-1:0] seed = 7;
 
-	always @(posedge clk) begin
+	always @(posedge clk or posedge rst) begin
 		for (int i=0; i<GRIDHEIGHT; i=i+1) begin : row
-			seed = seed * 1664525 + 1013904223;
+			seed <= seed * 1664525 + 1013904223;
 			for (int j=0; j<GRIDWIDTH; j=j+1) begin : col
-				rand_colony[i][j].state = condition'(seed[j]);
-				rand_colony[i][j].clan = NEUTRAL;
+				new_colony[i][j].state <= condition'(seed[j]);
+				new_colony[i][j].clan <= NEUTRAL;
 			end
 		end
 	end
 
 endmodule
 
+module StablePatternDetector(
+	input logic diff_vector[GRIDHEIGHT][GRIDWIDTH],
+	output logic execute
+);
+
+	always_comb begin
+		int total_temp = 0;
+		
+		for (int i=0; i<GRIDHEIGHT; i=i+1) begin
+			for (int j=0; j<GRIDWIDTH; j=j+1) begin
+				total_temp = total_temp + diff_vector[i][j];
+			end
+		end
+		
+		if (total_temp > ALLOWEDSTABLE) begin
+			execute = 1;
+		end else begin
+			execute = 0;
+		end
+	end
+	
+endmodule
+
 module Cell(
-    input logic clk,
-    input logic rst_n,
-    input logic [7:0] neighbors,
-    input entity default_state,
-    output entity data
+	input logic clk,
+	input logic rst_n,
+	input logic rst_grid,
+	input logic [7:0] neighbors,
+	input entity random_state,
+	output entity current_state
 );
 
 	entity nextstate;
@@ -152,81 +172,86 @@ module Cell(
 							  neighbors[6] +
 							  neighbors[7];
 	
-	always_ff @(posedge clk or negedge rst_n or posedge rst_def) begin
-		if (~rst_n) begin
-			nextstate <= default_state;
-		end
-		else if (rst_def) begin
-			nextstate.state <= alive;
-			nextstate.clan <= NEUTRAL;
+	always_ff @(posedge clk or negedge rst_n or posedge rst_grid) begin
+		if (~rst_n | rst_grid) begin
+			nextstate <= random_state;
 		end else begin
 
 			// Apply Game of Life rules
-			if (data.state == alive) begin
-				 // Cell is alive
-				 if (population < 2 || population > 3) begin
-					  // Overcrowding or loneliness, cell dies
-					  nextstate.state <= dead;
-				 end
+			if (current_state.state == alive) begin
+				// Cell is alive
+				if (population < 2 || population > 3) begin
+					// Overcrowding or loneliness, cell dies
+					nextstate.state <= dead;
+				end
 			end else begin
-				 // Cell is dead
-				 if (population == 3) begin
-					  // Exactly 3 neighbors, cell becomes alive
-					  nextstate.state <= alive;
-				 end
+				// Cell is dead
+				if (population == 3) begin
+					// Exactly 3 neighbors, cell becomes alive
+					nextstate.state <= alive;
+				end
 			end
 			
 			// tmp
 			nextstate.clan <= NEUTRAL;
 		end
-		data <= nextstate;
+		current_state <= nextstate;
 	end
 
 
 endmodule
 
-module GameOfLife (
-	input              MAX10_CLK1_50, // 50 MHz clock - PIN_P11
-	input logic [1:0]  KEY,   // Push button  - PIN_A7
+module GameOfLife(
+	input MAX10_CLK1_50,								// 50 MHz clock - PIN_P11
+	input logic [1:0] KEY,							// Push button  - PIN_A7
 	input logic [9:0] SW,
-	output logic [3:0] VGA_R,  // VGA Red Data   - PIN_AA1, V1, Y2, Y1
-	output logic [3:0] VGA_G,  // VGA Green Data - PIN_W1, T2, R2, R1
-	output logic [3:0] VGA_B,  // VGA Blue Data  - PIN_P1, T1, P4, N2
-	output logic       VGA_HS, // VGA hsync - PIN_N3
-	output logic       VGA_VS  // VGA vsync - PIN_N1
+	output logic [3:0] VGA_R,						// VGA Red current_state   - PIN_AA1, V1, Y2, Y1
+	output logic [3:0] VGA_G,						// VGA Green current_state - PIN_W1, T2, R2, R1
+	output logic [3:0] VGA_B,						// VGA Blue current_state  - PIN_P1, T1, P4, N2
+	output logic VGA_HS,								// VGA hsync - PIN_N3
+	output logic VGA_VS								// VGA vsync - PIN_N1
 );
 	
 	entity cells[GRIDHEIGHT][GRIDWIDTH];
+	entity cell_history[GRIDHEIGHT][GRIDWIDTH][CELLBUFFER];
+	logic cell_history_diff[GRIDHEIGHT][GRIDWIDTH];
+	
 	logic gol_clk;
 	logic rst_sync;
 	logic rst_n;
-	logic rst_def;
+	logic rst_grid;
 	
 	assign rst_sync = KEY[0];
 	assign rst_n = KEY[1];
-	assign rst_def = SW[0];
 	
 	clock_div gol_clock(
 		.clk(MAX10_CLK1_50), 
 		.clk_1hz(gol_clk)
 	);
 	
-	// generate random default state
-	entity rand_colony[GRIDHEIGHT][GRIDWIDTH];
 	
-	seed_generator rand_bit(
-		.clk(rst_sync),
-		.rand_colony(rand_colony)
+	// Reset on stable state
+	StablePatternDetector(
+		.diff_vector(cell_history_diff),
+		.execute(rst_grid)
 	);
 	
-
+	// Generate random state
+	entity new_colony[GRIDHEIGHT][GRIDWIDTH];
+	
+	GenerateSeed rand_bit(
+		.clk(rst_n),
+		.rst(rst_grid),
+		.new_colony(new_colony)
+	);
+	
 	genvar i, j;
-	generate		
+	generate
 		for (i=0; i<GRIDHEIGHT; i=i+1) begin : row
 			for (j=0; j<GRIDWIDTH; j=j+1) begin : col
 				logic [7:0] neighbors;
 				
-				// validate positions
+				// Validate neighbor positions
 				always_comb begin
 					if (i-1 >= 0 && j-1 >= 0)
 						neighbors[0] = cells[i-1][j-1].state;
@@ -244,12 +269,38 @@ module GameOfLife (
 						neighbors[6] = cells[i+1][j].state;
 					if (i+1 < GRIDHEIGHT && j+1 < GRIDWIDTH)
 						neighbors[7] = cells[i+1][j+1].state;
+						
+					// Cell-map difference
+					cell_history_diff[i][j] = cell_history[i][j][0].state & 
+														cell_history[i][j][1].state & 
+														cell_history[i][j][2].state &
+														cell_history[i][j][3].state & 
+														cell_history[i][j][4].state;
 				end
 				
-				Cell cell_inst(.clk(gol_clk), .rst_n(rst_n), .neighbors(neighbors), .default_state(rand_colony[i][j]), .data(cells[i][j]));
+				// Instantiate individual cells
+				Cell cell_inst(.clk(gol_clk), .rst_n(rst_n), .rst_grid(rst_grid), .neighbors(neighbors), 
+									.random_state(new_colony[i][j]), .current_state(cells[i][j]));
 			end
 		end
 	endgenerate
+	
+	// Save previous cell-maps (make into its own module?)
+	always_ff @(posedge gol_clk) begin
+		for (int k=0; k<CELLBUFFER-1; k=k+1) begin
+			for (int i=0; i<GRIDHEIGHT; i=i+1) begin
+				for (int j=0; j<GRIDWIDTH; j=j+1) begin
+					cell_history[i][j][k] <= cell_history[i][j][k+1];
+				end
+			end
+		end
+					
+		for (int i=0; i<GRIDHEIGHT; i=i+1) begin
+			for (int j=0; j<GRIDWIDTH; j=j+1) begin
+				cell_history[i][j][CELLBUFFER-1] <= cells[i][j];
+			end
+		end
+	end
 	
 	VGA_PRIMA (
 		.cells(cells),
